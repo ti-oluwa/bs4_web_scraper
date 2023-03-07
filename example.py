161a4670be02a5d2a2d9b3a239561db8f16247f0
ti-oluwa <105179638+ti-oluwa@users.noreply.github.com>
2023-03-07 18:04:28 +0100

    Updated example.py

--- example.py
+++ example.py
@@ -4,7 +4,7 @@ from scripts.help import bs4_credentials_template, available_translation_engines
 # create a scraper object
 bs4_scraper = BS4WebScraper(base_storage_dir='../', parser='html.parser',
                              log_filename='..\logs\\bs4_scraper.log')
-bs4_scraper.scrape('https://www.google.com/', 0)
+bs4_scraper.scrape('https://www.google.com/', 0, translate_to='fr')
 
 
 # prints the credentials format for the scraper
